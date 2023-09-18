9872daed9871268f689dd8386377c7bb1e813311
Sasox2 <saso.mp97@gmail.com>
2023-09-18 10:58:01 -0400

    review css

---
+++ class6.js
@@ -0,0 +1,4 @@
+var chipscost = 2.59
+var istrue = false
+nada = null
+document.write('John said, “This project is fun!”')
---
+++ jsfile.js
@@ -0,0 +1,15 @@
+document.write("Hello, this is my first js document"); //this is a coment
+alert("hello");
+
+let boat = 0
+if (boat ===3){
+    window.alert("you have the right number of boats");
+}
+else {
+    window.alert("You don't have the right number of boats");
+}
+//1 es piedra, 2 es papel 3 es tijera//
+let jugador = 0;
+let pc = 2;
+jugador = prompt("Elige: 1 para piedra, 2 para papel o 3 para trijera");
+alert("Elegiste" + jugador);
